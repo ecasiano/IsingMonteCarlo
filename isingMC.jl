8876d13b162220a8ee065a1e245d1486a248f825
ecasiano <Emanuel.Casiano-Diaz@uvm.edu>
2022-07-18 11:23:49 -0600

    NNN works well now

--- isingMC.jl
+++ isingMC.jl
@@ -11,7 +11,7 @@ end
 
 "------------------------------------------------------------------------------"
 
-function spin_flip!(ising_lattice, L, T, do_nnn=false)
+function spin_flip!(ising_lattice, L, T, J1, J2)
     "Chooses a spin randomly and proposes to flip it"
 
     # Sample row,column indices of spin
@@ -22,73 +22,71 @@ function spin_flip!(ising_lattice, L, T, do_nnn=false)
     E_old = 0
     E_new = 0
     if x != L
-        E_old -= ising_lattice[x,y]*ising_lattice[x+1,y]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[x+1,y]
+        E_old += J1*ising_lattice[x,y]*ising_lattice[x+1,y]
+        E_new += J1*(-1)*ising_lattice[x,y]*ising_lattice[x+1,y]
     else
-        E_old -= ising_lattice[x,y]*ising_lattice[1,y]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[1,y]
+        E_old += J1*ising_lattice[x,y]*ising_lattice[1,y]
+        E_new += J1*(-1)*ising_lattice[x,y]*ising_lattice[1,y]
     end
 
     if y != L
-        E_old -= ising_lattice[x,y]*ising_lattice[x,y+1]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[x,y+1]
+        E_old += J1*ising_lattice[x,y]*ising_lattice[x,y+1]
+        E_new += J1*(-1)*ising_lattice[x,y]*ising_lattice[x,y+1]
     else
-        E_old -= ising_lattice[x,y]*ising_lattice[x,1]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[x,1]
+        E_old += J1*ising_lattice[x,y]*ising_lattice[x,1]
+        E_new += J1*(-1)*ising_lattice[x,y]*ising_lattice[x,1]
     end
     
     if x != 1
-        E_old -= ising_lattice[x,y]*ising_lattice[x-1,y]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[x-1,y]
+        E_old += J1*ising_lattice[x,y]*ising_lattice[x-1,y]
+        E_new += J1*(-1)*ising_lattice[x,y]*ising_lattice[x-1,y]
     else
-        E_old -= ising_lattice[x,y]*ising_lattice[L,y]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[L,y]
+        E_old += J1*ising_lattice[x,y]*ising_lattice[L,y]
+        E_new += J1*(-1)*ising_lattice[x,y]*ising_lattice[L,y]
     end
 
     if y != 1
-        E_old -= ising_lattice[x,y]*ising_lattice[x,y-1]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[x,y-1]
+        E_old += J1*ising_lattice[x,y]*ising_lattice[x,y-1]
+        E_new += J1*(-1)*ising_lattice[x,y]*ising_lattice[x,y-1]
     else
-        E_old -= ising_lattice[x,y]*ising_lattice[x,L]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[x,L]
+        E_old += J1*ising_lattice[x,y]*ising_lattice[x,L]
+        E_new += J1*(-1)*ising_lattice[x,y]*ising_lattice[x,L]
     end
 
-    if do_nnn==true
-        # Compute energy between next-nearest-neighbor bonds before and after update
-        if x == L
-            bottom = 1
-        else
-            bottom = x+1
-        end
-
-        if y == L
-            right = 1
-        else
-            right = y+1
-        end
-        
-        if x == 1
-            top = L
-        else
-            top = x-1
-        end
-
-        if y == 1
-            left = L
-        else
-            left = y-1
-        end
-
-        E_old -= ising_lattice[x,y]*ising_lattice[bottom,right]
-        E_old -= ising_lattice[x,y]*ising_lattice[bottom,left]
-        E_old -= ising_lattice[x,y]*ising_lattice[top,left]
-        E_old -= ising_lattice[x,y]*ising_lattice[top,right]
-
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[bottom,right]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[bottom,left]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[top,left]
-        E_new -= (-1)*ising_lattice[x,y]*ising_lattice[top,right]
+    # Compute energy between next-nearest-neighbor bonds before and after update
+    if x == L
+        bottom = 1
+    else
+        bottom = x+1
     end
+
+    if y == L
+        right = 1
+    else
+        right = y+1
+    end
+    
+    if x == 1
+        top = L
+    else
+        top = x-1
+    end
+
+    if y == 1
+        left = L
+    else
+        left = y-1
+    end
+
+    E_old += J2*ising_lattice[x,y]*ising_lattice[bottom,right]
+    E_old += J2*ising_lattice[x,y]*ising_lattice[bottom,left]
+    E_old += J2*ising_lattice[x,y]*ising_lattice[top,left]
+    E_old += J2*ising_lattice[x,y]*ising_lattice[top,right]
+
+    E_new += J2*(-1)*ising_lattice[x,y]*ising_lattice[bottom,right]
+    E_new += J2*(-1)*ising_lattice[x,y]*ising_lattice[bottom,left]
+    E_new += J2*(-1)*ising_lattice[x,y]*ising_lattice[top,left]
+    E_new += J2*(-1)*ising_lattice[x,y]*ising_lattice[top,right]
 
     # Calculate energy difference between new and old configurations
     E_flip = E_new - E_old
@@ -106,7 +104,7 @@ end
 
 "------------------------------------------------------------------------------"
 
-function get_energy(ising_lattice, L, do_nnn=false)
+function get_energy(ising_lattice, L, J1,J2)
     "Computes energy of LxL Ising configuration"
 
     E = 0.0
@@ -115,49 +113,45 @@ function get_energy(ising_lattice, L, do_nnn=false)
         for j in 1:L
             # Accumulate 'horizontal' bond
             if i != L
-                E -= ising_lattice[i,j]*ising_lattice[i+1,j] 
+                E += J1 * ising_lattice[i,j]*ising_lattice[i+1,j] 
             else
-                E -= ising_lattice[i,j]*ising_lattice[1,j]
+                E += J1 * ising_lattice[i,j]*ising_lattice[1,j]
             end
              
             # Accumulate 'vertical' bond
             if j != L
-                E -= ising_lattice[i,j]*ising_lattice[i,j+1] 
+                E += J1 * ising_lattice[i,j]*ising_lattice[i,j+1] 
             else
-                E -= ising_lattice[i,j]*ising_lattice[i,1]
+                E += J1 * ising_lattice[i,j]*ising_lattice[i,1]
             end
 
-            if do_nnn==true
-
-                # Compute energy between next-nearest-neighbor bonds before and after update
-                if i == L
-                    bottom = 1
-                else
-                    bottom = i+1
-                end
-
-                if j == L
-                    right = 1
-                else
-                    right = j+1
-                end
-
-                if i == 1
-                    top = L
-                else
-                    top = i-1
-                end
-
-                if j == 1
-                    left = L
-                else
-                    left = j-1
-                end
-
-                E_nnn -= ising_lattice[i,j]*ising_lattice[bottom,right]
-                E_nnn -= ising_lattice[i,j]*ising_lattice[top,right]
-
+            # Compute energy between next-nearest-neighbor bonds before and after update
+            if i == L
+                bottom = 1
+            else
+                bottom = i+1
             end
+
+            if j == L
+                right = 1
+            else
+                right = j+1
+            end
+
+            if i == 1
+                top = L
+            else
+                top = i-1
+            end
+
+            if j == 1
+                left = L
+            else
+                left = j-1
+            end
+
+            E_nnn += J2 * ising_lattice[i,j]*ising_lattice[bottom,right]
+            E_nnn += J2 * ising_lattice[i,j]*ising_lattice[top,right]
 
         end
     end
@@ -184,15 +178,17 @@ end
 "---------------------------- Main (start) ------------------------------------"
 
 # Ising Lattice Parameters (want to make command line parameters later)
-L = 10
-T = 2.25
+L = 512
+T = 2.27
+J1 = -1.0
+J2 = 0.0
 K_B = 1
 seed = 0
 Random.seed!(seed)
 
 # Simulation parameters    (want to make command line parameters later)
 sweep = L*L
-bins_wanted = 100000
+bins_wanted = 10000
 bin_size = 1
 
 equilibration_steps = 100000
@@ -209,31 +205,19 @@ M = 0
 E_squared = 0
 M_squared = 0 
 
-# Set type of interaction
-# false: nearest-neighbor interaction 
-# true:  next-nearest-neighbor interaction
-do_nnn = true
-
-# Set interaction label for data file
-if do_nnn
-    interaction_label = "NNN"
-else
-    interaction_label = "NN"
-end
-
 # Open file to write data to
-open("L_$(L)_T_$(T)_seed_$(seed)_$(interaction_label).dat","w") do f
+open("L_$(L)_T_$(T)_J1_$(J1)_J2_$(J2)_seed_$(seed).dat","w") do f
     if (seed==0)
     write(f, "# L=$(L), T=$(T) \n")
     write(f, "# E      M        E^2        M^2\n")
     end
 
-# Open file to write configurations to
-open("L_$(L)_T_$(T)_spins_seed_$(seed)_$(interaction_label).dat","w") do g
-    if (seed==0)
-    write(g, "# L=$(L), T=$(T) \n")
-    write(g, "# Ising configs (vectorized) \n")
-    end
+# # Open file to write configurations to
+# open("L_$(L)_T_$(T)_J1_$(J1)_J2_$(J2)_spins_seed_$(seed).dat","w") do g
+#     if (seed==0)
+#     write(g, "# L=$(L), T=$(T) \n")
+#     write(g, "# Ising configs (vectorized) \n")
+#     end
 
 # Monte Carlo loop
 let m = 0              
@@ -245,15 +229,15 @@ let m = 0
                 m += 1
 
                 # Propose updates (just a spin flip in this case)
-                spin_flip!(ising_lattice,L,T,do_nnn)
+                spin_flip!(ising_lattice,L,T,J1,J2)
 
                 # Measure quantities of interest
                 if (m%(sweep*skip)==0 && m>equilibration_steps)
 
-                    global E += get_energy(ising_lattice,L,do_nnn)
+                    global E += get_energy(ising_lattice,L,J1,J2)
                     global M += get_magnetization(ising_lattice,L)
 
-                    global E_squared += get_energy(ising_lattice,L)^2
+                    global E_squared += get_energy(ising_lattice,L,J1,J2)^2
                     global M_squared += get_magnetization(ising_lattice,L)^2
 
                     measurement_ctr += 1
@@ -263,13 +247,13 @@ let m = 0
                         # Write data if accumulator has 'bin_size no. of samples
                         write(f, "$(E/bin_size)    $(M/bin_size)     $(E_squared/bin_size)     $(M_squared/bin_size)\n")
 
-                        # Write spin configs if accumulator has 'bin_size no. of samples
-                        for i in 1:L
-                            for j in 1:L
-                                write(g,"$(ising_lattice[i,j]) ")
-                            end
-                        end
-                        write(g,"\n")
+                        # # Write spin configs if accumulator has 'bin_size no. of samples
+                        # for i in 1:L
+                        #     for j in 1:L
+                        #         write(g,"$(ising_lattice[i,j]) ")
+                        #     end
+                        # end
+                        # write(g,"\n")
 
                         bins_written += 1
                         if (bins_written%1000==0)
@@ -285,10 +269,13 @@ let m = 0
 
                         
                     end # end of writing-to-disk if-statement
-                    end # end of writing-spins-to-disk if-statement 
+                    # end # end of writing-spins-to-disk if-statement 
                 end # end of perform-measurement-and-accumulate if-statemenet
             end # end of main Monte Carlo (while) loop
         end # end of let measurement_ctr
     end # end of let bins_written
+# Close data files
 end # end of let m
+close(f)
+#close(g)
 end
